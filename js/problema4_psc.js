9235aab06aa0678432900665f2efb9d1751d7332
Alberto Montero Molina <monteromolinaalberto@gmail.com>
2021-04-09 23:58:05 -0500

    corrijo js problema 4

--- js/problema4_psc.js
+++ js/problema4_psc.js
@@ -30,8 +30,7 @@ function calculo(){
                         var utilidad = valor_a * 0.20;
                     }
                 }
-            }
-            
+            }      
         }
     }
 
